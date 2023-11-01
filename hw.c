62746bcca25ed445459f7be6be65e742f7232494
Gabriel Gibb <gabrielgibb1@gmail.com>
2023-11-01 16:16:58 -0700

    can handle user input and initialize some variables

--- hw.c
+++ hw.c
@@ -1,23 +1,110 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Memory {
+ int address, data;
+};
+struct PageTable {
+ int v_page_num, valid_bit, dirty_bit, page_num; //, time_stamp; this was in discussion code but not in any instructions
+};
+struct Memory main_memory[32];
+struct Memory virtual_memory[128];
+struct PageTable p_table[16];
+
 int fifo = 0, lru = 0;
 
-void loop() {
-    char* input[100];
-    char** args;
-    do {
-        printf("> ");
-        fgets(input, 80, stdin);
-        args = tokenize((char *) input);
-        if (strcmp(args[0], "quit") == 0)
-            exit(0);
-        execute(args);
-    } while (1);
+//Main calls this so we can initialize whatever we need here
+//Unsure if this is correct
+void init(){
+    //Initialize main_memory
+    for(int i = 0; i < 128; i++){
+        virtual_memory[i].address = i;
+        virtual_memory[i].data = -1;
+    }
+
+    //initialize virtual memory (idk what to do)
+    for(int i = 0; i < 32; i++){
+        // virtual_memory[i].address = i;
+        main_memory[i].data = -1;
+    }
+
+    //initialize p_table 
+    for (int i = 0; i < 16; i++){
+        p_table[i].v_page_num = i;
+        p_table[i].valid_bit = 0;
+        p_table[i].dirty_bit = 0;
+        p_table[i].page_num = i;
+    }
+} 
+
+void pageFault(){
+    printf("A Page Fault Has Occurred\n");
 }
 
-int main(int argc, char** argv) {
-    if (argv[1] == NULL || strcmp (argv[1], "FIFO") == 0)
+//right now input is routed to call these functions, these print statements are for your understanding
+void read(int vAddress){
+    printf("READ vAddress %d\n", vAddress);
+}
+
+void write(int vAddress, int num){
+    printf("WRITE vAddress: %d, num: %d\n", vAddress, num);
+}
+
+void showmain(int ppn){
+    printf("SHOWMAIN ppn %d\n", ppn);
+}
+
+void showptable(){ //DONE
+    for (int i = 0; i < 16; i++){
+        printf("%d:%d:%d:%d\n", p_table[i].v_page_num, p_table[i].valid_bit, p_table[i].dirty_bit, p_table[i].page_num);
+    }
+}
+
+
+//Handle user input
+void loop() { //DONE
+    char input[100];
+    char* token;
+    while(1) {
+        printf("> ");
+        fgets(input, 80, stdin);
+        input[strcspn(input, "\n")] = 0;
+        token = strtok(input, " ");
+        if (strcmp(token, "quit") == 0){
+            exit(0);
+        }
+        else if (strcmp(token, "read") == 0){
+            token = strtok(NULL, " ");
+            int vAddress = atoi(token);
+            read(vAddress);
+        }
+        else if (strcmp(token, "write") == 0){
+            token = strtok(NULL, " ");
+            int vAddress = atoi(token);
+            token = strtok(NULL, " ");
+            int num = atoi(token);
+            write(vAddress, num);
+        }
+        else if (strcmp(token, "showmain") == 0){
+            token = strtok(NULL, " ");
+            int ppn = atoi(token);
+            showmain(ppn);
+        }
+        else if (strcmp(token, "showptable") == 0){
+            showptable();
+        }
+    };
+}
+
+int main(int argc, char** argv) { //DONE
+    //fifo is chosen given no args or if chosen
+    if (argv[1] == NULL || strcmp (argv[1], "FIFO") == 0){
         fifo = 1;
-    else if (strcmp (argv[1], "LRU") == 0)
+    }
+    else if (strcmp (argv[1], "LRU") == 0){ 
         lru = 1;
+    }
     init();
     loop();
     return 0;
